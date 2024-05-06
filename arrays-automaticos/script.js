a4a252deb69e4f6090e7d5eb7d577b1c3eed7bba
Viniciusmendes21 <101270160+Viniciusmendes21@users.noreply.github.com>
2024-05-06 17:06:06 -0300

    Add files via upload

---
+++ arrays-automaticos/script.js
@@ -0,0 +1,48 @@
+console.log("Usando o forEach", "------------------------------------------------------")
+
+dados.forEach(
+    (elemento) => {
+        console.log(elemento.nome)
+    }
+);
+
+console.log("Usando o while", "------------------------------------------------------")
+
+let indice = 0;
+
+while(indice < dados.length) {
+    console.log("A posição de", dados[indice].nome, "é", dados[indice].posicao);
+    indice++
+}
+
+console.log("Usando o for", "------------------------------------------------------")
+for(let i = 0 ; i < dados.length; i++) {
+    console.log(`A altura de ${dados[i].nome} é ${dados[i].altura}`);
+}
+
+console.log("Usando o for.....of", "------------------------------------------------------")
+for (const elemento of dados) {
+    console.log(`O Nome Completo de ${elemento.nome} é  ${elemento.nome_completo}`);
+}
+
+//const atleta = dados[15];
+
+const container = document.getElementById("conteudo");
+
+dados.forEach(
+    (atleta) => {
+        container.innerHTML += `
+    <div class="card">
+    <div class="imagem">
+        <img src=${atleta.imagem} alt='foto de ${atleta.nome}'>
+    </div>
+        <p class="posicao">${atleta.posicao}</p>
+        <p class="nome">${atleta.nome}</p>
+        <p class="descri">${atleta.descricao}</p>
+        <p class="nascimento">${atleta.nascimento}</p>
+    </div>
+`
+    }
+);
+
+
